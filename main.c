3602db3e53af830b608a2325e261ba233b12fb46
nerya0001 <nerya0001@gmail.com>
2021-11-01 07:09:02 -0700

    main is done

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "NumClass"
+
+int main() {
+  int a, b;
+
+  printf("Please enter two positive integers:\n");
+  scanf("%d%d\n", a, b);
+
+  //isArmstrong
+  printf("The Armstrong numbers are: ")
+  for (int i = a; i <= b; i++) {
+    if (isArmstrong(i) == 1) {
+      printf("%d ", i);
+    }
+  }
+  //isPalindrome
+  printf("\nThe Palindromes are: ")
+  for (int i = a; i <= b; i++) {
+    if (isPalindrome(i) == 1) {
+      printf("%d ", i);
+    }
+  }
+  //isprime
+  printf("\nThe Prime numbers are: ")
+  for (int i = a; i <= b; i++) {
+    if (isPrime(i) == 1) {
+      printf("%d ", i);
+    }
+  }
+  //isStrong
+  printf("\nThe Strong numbers are: ")
+  for (int i = a; i <= b; i++) {
+    if (isStrong(i) == 1) {
+      printf("%d ", i);
+    }
+  }
+
+
+}
