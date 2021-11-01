365be9fbe02e60c5f844652eee3e6798713d1cf9
nerya bigon <nerya0001@gmail.com>
2021-11-01 04:35:09 -0700

    isArmstrong is done

--- advancedClassificationloop.c
+++ advancedClassificationloop.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+int dig(int x) {
+  int count = 0;
+  while (x > 0) {
+    count++;
+    x /= 10;
+  }
+  return count;
+}
 
 //isPalindrome
+int isPalindrome(int n) {
+
+}
 
 //isArmstrong
+int isArmstrong(int n) {
+  int ncopy = n, sum = 0;
+  int digits = dig(n);
+  while (n > 0) {
+    sum += (int)(pow((n % 10), digits));
+    n /= 10;
+  }
+  if (sum == ncopy) {
+    return true;
+  }
+  return false;
+}
 
-//both functions need to be implemented with loops
+int main() {
+  int n = 407;
+  int ans = isArmstrong(n);
+  if (ans == 1) {
+    printf("n is armstrong\n");
+  }
+}
