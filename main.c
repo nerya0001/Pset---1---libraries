16345b8158f007081396338f94ce2a4985d8c037
nerya0001 <nerya0001@gmail.com>
2021-11-02 09:51:15 -0700

    space

--- main.c
+++ main.c
@@ -11,28 +11,29 @@ int main() {
   printf("The Armstrong numbers are:");
   for (int i = a; i <= b; i++) {
     if (isArmstrong(i) == 1) {
-      printf(" %d ", i);
+      printf(" %d", i);
     }
   }
   //isPalindrome
   printf("\nThe Palindromes are:");
   for (int i = a; i <= b; i++) {
     if (isPalindrome(i) == 1) {
-      printf(" %d ", i);
+      printf(" %d", i);
     }
   }
   //isprime
   printf("\nThe Prime numbers are:");
   for (int i = a; i <= b; i++) {
     if (isPrime(i) == 1) {
-      printf(" %d ", i);
+      printf(" %d", i);
     }
   }
   //isStrong
   printf("\nThe Strong numbers are:");
   for (int i = a; i <= b; i++) {
     if (isStrong(i) == 1) {
-      printf(" %d ", i);
+      printf(" %d", i);
     }
   }
+  printf("\n");
 }
