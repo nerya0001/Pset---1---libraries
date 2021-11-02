4086f603b73baf902db9d55e01e3333ee965c08a
nerya0001 <nerya0001@gmail.com>
2021-11-02 09:40:54 -0700

    trying to change spaces

--- main.c
+++ main.c
@@ -8,31 +8,31 @@ int main() {
   }
   while (a <= 0 || b <= 0);//checking that the input is as it should be
   //isArmstrong
-  printf("The Armstrong numbers are: ");
+  printf("The Armstrong numbers are:");
   for (int i = a; i <= b; i++) {
     if (isArmstrong(i) == 1) {
-      printf("%d ", i);
+      printf(" %d ", i);
     }
   }
   //isPalindrome
-  printf("\nThe Palindromes are: ");
+  printf("\nThe Palindromes are:");
   for (int i = a; i <= b; i++) {
     if (isPalindrome(i) == 1) {
-      printf("%d ", i);
+      printf(" %d ", i);
     }
   }
   //isprime
-  printf("\nThe Prime numbers are: ");
+  printf("\nThe Prime numbers are:");
   for (int i = a; i <= b; i++) {
     if (isPrime(i) == 1) {
-      printf("%d ", i);
+      printf(" %d ", i);
     }
   }
   //isStrong
-  printf("\nThe Strong numbers are: ");
+  printf("\nThe Strong numbers are:");
   for (int i = a; i <= b; i++) {
     if (isStrong(i) == 1) {
-      printf("%d ", i);
+      printf(" %d ", i);
     }
   }
   printf("\n");
